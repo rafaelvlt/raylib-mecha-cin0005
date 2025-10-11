0496676406231d8a84a65a41491733b0d49605b7
rafaelvlt <rafael_alebb@hotmail.com>
2025-10-11 11:54:11 -0300

    docs: made comments about the new raygui implementation to help others work with it

--- src/states.c
+++ src/states.c
@@ -79,38 +79,39 @@ void DrawMenuScreen(Camera3D* camera, EntityManager* entityManager, MenuButton* 
 
 
     //------- 2D GUI ----------
+    
+    // ------- Title Specs + Draw ----------
     const float titleX = SCREEN_WIDTH/2 - MeasureText(GAME_TITLE, 60)/2;
     const float titleY = 25;
     const float titleFontSize = 60;
     DrawText(GAME_TITLE, titleX, titleY, titleFontSize, WHITE);
     
-    
+    // ------- Menu Panel Specs ----------
     const float panelWidth = SCREEN_WIDTH / 3.0f;
     const float panelHeight = SCREEN_HEIGHT * 0.75f;
     const float panelX = SCREEN_WIDTH * 0.045f;
     const float panelY = (SCREEN_HEIGHT - panelHeight) / 1.3f;
+    // ------- Draw Menu Panel ----------
     Rectangle menuPanel = {panelX, panelY, panelWidth, panelHeight};
     DrawRectangleRec(menuPanel, Fade(BLACK, 0.75f));
     DrawRectangleLinesEx(menuPanel, 4, GREEN);
-
-    
     GuiGroupBox(menuPanel, "");
-    int originalTextSize = GuiGetStyle(DEFAULT, TEXT_SIZE);
-    int originalTextAlign = GuiGetStyle(BUTTON, TEXT_ALIGNMENT);
 
+    //------- Buttons Specs ----------
     GuiSetStyle(DEFAULT, TEXT_SIZE, 30);           
     GuiSetStyle(BUTTON, TEXT_ALIGNMENT, TEXT_ALIGN_CENTER);
     const float buttonWidth = panelWidth * 0.8f;
     const float buttonHeight = 50.0f;
     const float buttonSpacing = 20.0f;
     const float buttonX = menuPanel.x + (menuPanel.width - buttonWidth) / 2.0f;
+
     Rectangle startButtonRect = { buttonX, menuPanel.y + 60, buttonWidth, buttonHeight };
     Rectangle loadoutButtonRect = { buttonX, startButtonRect.y + buttonHeight + buttonSpacing, buttonWidth, buttonHeight };
     Rectangle optionsButtonRect = { buttonX, loadoutButtonRect.y + buttonHeight + buttonSpacing, buttonWidth, buttonHeight };
     Rectangle creditsButtonRect = { buttonX, optionsButtonRect.y + buttonHeight + buttonSpacing, buttonWidth, buttonHeight };
     Rectangle exitButtonRect = { buttonX, creditsButtonRect.y + buttonHeight + buttonSpacing, buttonWidth, buttonHeight };
 
-    // Logic to
+    // Checks if the button is pressed and draw them too
     if (GuiButton(startButtonRect, "START GAME")) *menuButtonSelected = START_GAME;
     if (GuiButton(loadoutButtonRect, "LOADOUT"))  *menuButtonSelected = LOADOUT;
     if (GuiButton(optionsButtonRect, "OPTIONS"))  *menuButtonSelected = OPTIONS;
