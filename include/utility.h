b60940bb356c26aeb03b46273591d1825f865e96
rafaelvlt <rafael_alebb@hotmail.com>
2025-10-13 18:48:44 -0300

    refactor: main is now compliant with new project architecture and best practices

--- include/utility.h
+++ include/utility.h
@@ -1,7 +1,29 @@
 #ifndef UTILITY_H_11102025
 #define UTILITY_H_11102025
 
+#include <raylib.h>
+#include <stdlib.h>
+#include "resource_manager.h"
+#include "state_manager.h"
+#include "ecs/ecs_components.h"
+#include "ecs/ecs_entitymanager.h"
+#include "ecs/ecs_systems.h"
+
 #define SCREEN_WIDTH 1280
 #define SCREEN_HEIGHT 720
 #define GAME_TITLE "MECHA GAME"
+
+//Cores
+#define GREENISH_BLACK  CLITERAL(Color){ 0, 25, 0, 255 }
+
+struct Systems{
+    EntityManager entityManager;
+    StateManager stateManager;
+    ResourceManager resourceManager;
+
+    //TBD Below
+    //EventSystem eventSystem  
+};
+
+
 #endif
--- src/main.c
+++ src/main.c
@@ -1,134 +1,47 @@
 #include <raylib.h>
 #include <raymath.h>
 #include "utility.h"
-#include "ecs_systems.h"       
-#include "ecs_entitymanager.h" 
-#include "states.h"
+#include "state_manager.h"
+#include "resource_manager.h"
+#include "ecs/ecs_components.h"
+#include "ecs/ecs_entitymanager.h"
+#include "ecs/ecs_systems.h"
+
 
 int main(void) {
     // --- Window Initialization ---
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, GAME_TITLE);
+    InitAudioDevice();
     
-    InitAudioDevice();// Initialize audio device
+    struct Systems systems = {0};
+    InitResourceManager(&systems.resourceManager);
+    InitStateManager(&systems, SCREEN_TITLE);
 
-    // Camera Initialization (Same as Raylib Examples)
-    Camera camera = { 0 };
-    camera.position = (Vector3){ 0.0f, 2.0f, 4.0f };
-    camera.target = (Vector3){ 0.0f, 2.0f, 0.0f }; 
-    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
-    camera.fovy = 60.0f;
-    camera.projection = CAMERA_PERSPECTIVE;
-
-    // --- MENU -------
-    // State Machine Definition
-    GameScreen gameScreen = TITLE;
-    MenuButton menuButtonSelected = NONE;
-    Music music = LoadMusicStream("C:/raylib-mecha-cin0005/The King of fighters 96 16   Protector Demo 1.mp3");
-    float timePlayed = 0.0f;
-    music.looping = false;
-    PlayMusicStream(music);
-
-    // TITLE/MENU Variables
-    int framesCounter = 0;
-    
-    // --- ECS Initialization ---
-    EntityManager entityManager;
-    InitEntityManager(&entityManager);
-
-    DisableCursor();
     SetTargetFPS(60);
 
-    // Test variables for the Menu Orbit Target
-    // An Asset Manager will be made for this in the future.
-    Model playerModel = LoadModel("resources\\models\\player\\mechafullmenu.obj");
-    //--------- Split Screen(MENU) -----------
-    RenderTexture splitScreen[2]; 
-    splitScreen[0] = LoadRenderTexture(SCREEN_WIDTH/2, SCREEN_HEIGHT);
-    splitScreen[1] = LoadRenderTexture(SCREEN_WIDTH/2, SCREEN_HEIGHT);
+    //Title + Menu Music
+    Music* MainMenuMusicPtr = GetMusic(&(systems.resourceManager), MUSIC_ID_MENU);
+    
     // --- Game Loop ---
     while (!WindowShouldClose()) {
-        // Delta Time (Attribute to no give a warning to the compiler about not being used)
-        [[maybe_unused]] float deltaTime = GetFrameTime();
 
         // Update Phase
-        UpdateMusicStream(music);      // Update music buffer with new stream data
-        switch (gameScreen)
-        {
-            case TITLE:
-            {
-                framesCounter++;    // Count frames
-
-                // Wait for 4 seconds (240 frames) before jumping to MENU screen
-                if (framesCounter > 240)
-                {
-                    gameScreen = MENU;
-                    InitMenuScreen(&camera, &entityManager, &playerModel);
-                }
-            } break;
-            case MENU:
-            {
-                UpdateMenuScreen(&gameScreen, &camera, &menuButtonSelected);
-            } break;
-            case FIRST_LEVEL:
-            {
-                // TODO
-                if (IsKeyPressed(KEY_ENTER))
-                {
-                    gameScreen = MENU;
-                }
-            } break;
-            case CREDITS_SCREEN:
-            {
-                // TODO
-
-                // Press enter to return to MENU screen
-                if (IsKeyPressed(KEY_ENTER))
-                {
-                    gameScreen = MENU;
-                }
-            } break;
-            default: break;
-        }
+        UpdateMusicStream(*MainMenuMusicPtr);      // Update music buffer with new stream data
+        UpdateStateManager(&systems);
 
         // Drawing Phase
         BeginDrawing();
             ClearBackground(BLACK);
-            switch (gameScreen)
-            {
-                case TITLE:
-                {
-                    DrawTitleScreen(framesCounter);
-                } break;
-                case MENU:
-                {
-                    DrawMenuScreen(&camera, &entityManager, &menuButtonSelected, splitScreen, framesCounter);
-                } break;
-                case FIRST_LEVEL:
-                {
-                    //TODO
-
-                    // Dummy text for testing
-                    DrawText("FIRST LEVEL", SCREEN_WIDTH/2 - MeasureText(GAME_TITLE, 100)/2, SCREEN_HEIGHT/2, 100, WHITE);
-                    DrawText("Press Enter to go to Menu", SCREEN_WIDTH/2 - MeasureText("Press Enter to go to Menu", 30)/2, SCREEN_HEIGHT/2 + 100, 30, WHITE);
-
-                } break;
-                case CREDITS_SCREEN:
-                {
-                    DrawCreditsScreen();
-                } break;
-                default: break;
-            }
+            DrawStateManager(&systems);
         EndDrawing();
     }
 
-    // ---  Cleaning Memory ---
-    UnloadModel(playerModel);
-    UnloadMusicStream(music);          // Unload music stream buffers from RAM
+    //Shutdown struct Systems
+    ShutdownStateManager(&systems);
+    ClearEntityManager(&systems.entityManager);
+    ShutdownResourceManager(&systems.resourceManager);
+
     CloseAudioDevice();     // Close audio device (music streaming is automatically stopped)
-    
-    UnloadRenderTexture(splitScreen[0]);
-    UnloadRenderTexture(splitScreen[1]);
     CloseWindow();
     return 0;
 }
-
