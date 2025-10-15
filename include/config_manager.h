5d581c3a02a26b2a9693ba6861e552df2297afbd
YtBelll <ytbell@outlook.com>
2025-10-15 20:45:39 -0300

    feat: criando o sistema de configurações

--- include/config_manager.h
+++ include/config_manager.h
@@ -1,5 +1,6 @@
 #ifndef CONFIG_MANAGER_H
 #define CONFIG_MANAGER_H
+#define OPTION_LINES 5
 
 #include <raylib.h>
 
@@ -19,10 +20,36 @@ typedef enum {
 
 // Struct to hold configuration values
 typedef struct {
+    int language; //0 = EN, 1 = PT-BR
     float audioVolume;
     Vector2 screenResolution;
     bool fullscreen;
 } ConfigManager;
+
+typedef struct OptionsData {
+    Font* creditsFont;
+    int selectedOption[2]; // [line, option]
+    int rangeOptions[OPTION_LINES]; //number of options in each line
+} OptionsData;
+
+typedef enum options {
+    OPTION_LANGUAGE = 0,
+    OPTION_SCREEN_RESOLUTION,
+    OPTION_FULLSCREEN,
+    OPTION_AUDIO_VOLUME,
+    OPTION_BACK_TO_MENU
+} Option;
+
+typedef enum Language {
+    LANGUAGE_EN = 0,
+    LANGUAGE_PT_BR
+} Language;
+
+typedef enum ScreenResolution {
+    RESOLUTION_1280x720 = 0,
+    RESOLUTION_1600x900,
+    RESOLUTION_1920x1080
+} ScreenResolution;
 
 void InitConfigManager(struct Systems* systems);
 void UpdateConfigManager(struct Systems* systems);
