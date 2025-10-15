ee6e073484dd8328b79cff6ad69f85d2692b52eb
YtBelll <ytbell@outlook.com>
2025-10-15 19:43:40 -0300

    refactor: config_manager criado pra facilitar a configuração

---
+++ include/config_manager.h
@@ -0,0 +1,35 @@
+#ifndef CONFIG_MANAGER_H
+#define CONFIG_MANAGER_H
+
+#include <raylib.h>
+
+struct Systems;
+
+/*******************
+ WORK IN PROGRESS  * 
+ *******************/
+
+// Enum for configuration IDs (add more as needed)
+typedef enum {
+    CONFIG_ID_AUDIO_VOLUME = 0,
+    CONFIG_ID_SCREEN_RESOLUTION,
+    CONFIG_ID_FULLSCREEN,
+    CONFIG_ID_COUNT
+} ConfigID;
+
+// Struct to hold configuration values
+typedef struct {
+    float audioVolume;
+    Vector2 screenResolution;
+    bool fullscreen;
+} ConfigManager;
+
+void InitConfigManager(struct Systems* systems);
+void UpdateConfigManager(struct Systems* systems);
+void ShutdownConfigManager();
+
+float GetAudioVolume(ConfigManager* configManager);
+Vector2 GetScreenResolution(ConfigManager* configManager);
+bool GetFullscreen(ConfigManager* configManager);
+
+#endif
