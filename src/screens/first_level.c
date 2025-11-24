a296e63d770d65782479dec0e6ed6d583581587c
HebSP <hsp2@cin.ufpe.br>
2025-11-24 15:31:06 -0300

    style: identation fixes in first_level

--- src/screens/first_level.c
+++ src/screens/first_level.c
@@ -45,81 +45,81 @@ static void DrawTargetDebug(struct Systems* systems) {
 
 void InitFirstLevelScreen(struct Systems* systems, FirstLevelData* data)
 {
-    // 1. Reset ECS state
-    InitEntityManager(&systems->entityManager);
+  // 1. Reset ECS state
+  InitEntityManager(&systems->entityManager);
 
-    // ---------------------------------------------------------
-    // Player Entity Setup
-    // ---------------------------------------------------------
-    Entity player = CreateEntity(&systems->entityManager);
+  // ---------------------------------------------------------
+  // Player Entity Setup
+  // ---------------------------------------------------------
+  Entity player = CreateEntity(&systems->entityManager);
 
-    AddTransformComponent(&systems->entityManager, player, (Vector3){ 0.0f, 0.0f, 0.0f });
-    AddPhysicsComponent(&systems->entityManager, player, (Vector3){ 0.0f, 0.0f, 0.0f }, 0.90f);
+  AddTransformComponent(&systems->entityManager, player, (Vector3){ 0.0f, 0.0f, 0.0f });
+  AddPhysicsComponent(&systems->entityManager, player, (Vector3){ 0.0f, 0.0f, 0.0f }, 0.90f);
 
-    // Connects camera to player control. Sensitivity: 0.003, MaxSpeed: 15.0, TurnSpeed: 2.0
-    AddPlayerControlComponent(&systems->entityManager, player, &data->camera);
-    
-    BoundingBox playerBounds = (BoundingBox){(Vector3){-1,-1,-1}, (Vector3){1,2,1}};
-    AddCollisionComponent(&systems->entityManager, player, playerBounds, false, false);
+  // Connects camera to player control. Sensitivity: 0.003, MaxSpeed: 15.0, TurnSpeed: 2.0
+  AddPlayerControlComponent(&systems->entityManager, player, &data->camera);
+  
+  BoundingBox playerBounds = (BoundingBox){(Vector3){-1,-1,-1}, (Vector3){1,2,1}};
+  AddCollisionComponent(&systems->entityManager, player, playerBounds, false, false);
 
-    // ---------------------------------------------------------
-    // Weapon Setup 
-    // ---------------------------------------------------------
-    
-    // --- Left Weapon ---
-    Entity weaponLeft = CreateEntity(&systems->entityManager);
-    Vector3 offsetL = { -1.70f, 5.50f, -1.50f };
-    
-    AddTransformComponent(&systems->entityManager, weaponLeft, Vector3Zero());
-    AddAttachmentComponent(&systems->entityManager, weaponLeft, player, offsetL, QuaternionIdentity());
-    AddWeaponComponent(&systems->entityManager, weaponLeft, WEAPON_PULSE_LASER, 0.5f, 150.0f, 5.0f, 500.0f, 0.0f, SOUND_ID_COUNT, MODEL_ID_PROJECTILE_PULSE_LASER);
+  // ---------------------------------------------------------
+  // Weapon Setup 
+  // ---------------------------------------------------------
+  
+  // --- Left Weapon ---
+  Entity weaponLeft = CreateEntity(&systems->entityManager);
+  Vector3 offsetL = { -1.70f, 5.50f, -1.50f };
+  
+  AddTransformComponent(&systems->entityManager, weaponLeft, Vector3Zero());
+  AddAttachmentComponent(&systems->entityManager, weaponLeft, player, offsetL, QuaternionIdentity());
+  AddWeaponComponent(&systems->entityManager, weaponLeft, WEAPON_PULSE_LASER, 0.5f, 150.0f, 5.0f, 500.0f, 0.0f, SOUND_ID_COUNT, MODEL_ID_PROJECTILE_PULSE_LASER);
 
-    // --- Right Weapon ---
-    Entity weaponRight = CreateEntity(&systems->entityManager);
-    Vector3 offsetR = { 1.70f, 5.50f, -1.50f }; // Inverted X
+  // --- Right Weapon ---
+  Entity weaponRight = CreateEntity(&systems->entityManager);
+  Vector3 offsetR = { 1.70f, 5.50f, -1.50f }; // Inverted X
 
-    AddTransformComponent(&systems->entityManager, weaponRight, Vector3Zero());
-    AddAttachmentComponent(&systems->entityManager, weaponRight, player, offsetR, QuaternionIdentity());
-    AddWeaponComponent(&systems->entityManager, weaponRight, WEAPON_PULSE_LASER, 0.5f, 150.0f, 5.0f, 500.0f, 0.0f, SOUND_ID_COUNT, MODEL_ID_PROJECTILE_PULSE_LASER);
+  AddTransformComponent(&systems->entityManager, weaponRight, Vector3Zero());
+  AddAttachmentComponent(&systems->entityManager, weaponRight, player, offsetR, QuaternionIdentity());
+  AddWeaponComponent(&systems->entityManager, weaponRight, WEAPON_PULSE_LASER, 0.5f, 150.0f, 5.0f, 500.0f, 0.0f, SOUND_ID_COUNT, MODEL_ID_PROJECTILE_PULSE_LASER);
 
-    // ---------------------------------------------------------
-    // Weapon Control System (Loadout)
-    // ---------------------------------------------------------
-    AddWeaponControlComponent(&systems->entityManager, player, AIM_MODE_CAMERA);
-    
-    WeaponControlComponent* ctrl = &systems->entityManager.weaponControlComponents[player];
+  // ---------------------------------------------------------
+  // Weapon Control System (Loadout)
+  // ---------------------------------------------------------
+  AddWeaponControlComponent(&systems->entityManager, player, AIM_MODE_CAMERA);
+  
+  WeaponControlComponent* ctrl = &systems->entityManager.weaponControlComponents[player];
 
-    // Link entities to slots and groups
-    ctrl->weaponsSlots[0] = weaponLeft;
-    ctrl->weaponsGroupMap[0] = 0; // Group 1
-    
-    ctrl->weaponsSlots[1] = weaponRight;
-    ctrl->weaponsGroupMap[1] = 0; // Group 1
+  // Link entities to slots and groups
+  ctrl->weaponsSlots[0] = weaponLeft;
+  ctrl->weaponsGroupMap[0] = 0; // Group 1
+  
+  ctrl->weaponsSlots[1] = weaponRight;
+  ctrl->weaponsGroupMap[1] = 0; // Group 1
 
-    ctrl->activeGroup[0] = true;
+  ctrl->activeGroup[0] = true;
 
-    // ---------------------------------------------------------
-    // Enemy Entity Setup
-    // ---------------------------------------------------------
-    
-    //ajustei a função para criar vários inimigos de forma mais simples
-    Vector3 scoutPoints1[4] = { {20.0f,0.0f,70.0f}, {-20.0f,0.0f,70.0f},{-20.0f,0.0f,30.0f}, {20.0f,0.0f,30.0f} }; //square patrol
-    Vector3 scoutPoints2[2] = { {15.0f,0.0f,80.0f}, {-15.0f,0.0f,80.0f} }; //line patrol
+  // ---------------------------------------------------------
+  // Enemy Entity Setup
+  // ---------------------------------------------------------
+  
+  //ajustei a função para criar vários inimigos de forma mais simples
+  Vector3 scoutPoints1[4] = { {20.0f,0.0f,70.0f}, {-20.0f,0.0f,70.0f},{-20.0f,0.0f,30.0f}, {20.0f,0.0f,30.0f} }; //square patrol
+  Vector3 scoutPoints2[2] = { {15.0f,0.0f,80.0f}, {-15.0f,0.0f,80.0f} }; //line patrol
 
-    // Creating multiple scouts with patrol points
-    createEnemyScout(&systems->resourceManager,&systems->entityManager, scoutPoints1[0], scoutPoints1, 4);
-    createEnemyScout(&systems->resourceManager,&systems->entityManager, scoutPoints2[0], scoutPoints2, 2);
-    
-    createEnemyCombatent(&systems->resourceManager,&systems->entityManager, (Vector3){ 0.0f, 0.0f, 50.0f });
+  // Creating multiple scouts with patrol points
+  createEnemyScout(&systems->resourceManager,&systems->entityManager, scoutPoints1[0], scoutPoints1, 4);
+  createEnemyScout(&systems->resourceManager,&systems->entityManager, scoutPoints2[0], scoutPoints2, 2);
+  
+  createEnemyCombatent(&systems->resourceManager,&systems->entityManager, (Vector3){ 0.0f, 0.0f, 50.0f });
 
-    // ---------------------------------------------------------
-    // Camera & Input Setup
-    // ---------------------------------------------------------
-    data->camera.position = (Vector3){ 0.0f, 2.5f, 0.0f };
-    data->camera.target = (Vector3){ 0.0f, 2.5f, 1.0f };
-    data->camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
-    data->camera.fovy = 60.0f;
-    data->camera.projection = CAMERA_PERSPECTIVE;
+  // ---------------------------------------------------------
+  // Camera & Input Setup
+  // ---------------------------------------------------------
+  data->camera.position = (Vector3){ 0.0f, 2.5f, 0.0f };
+  data->camera.target = (Vector3){ 0.0f, 2.5f, 1.0f };
+  data->camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };
+  data->camera.fovy = 60.0f;
+  data->camera.projection = CAMERA_PERSPECTIVE;
 
   DisableCursor(); 
 }
