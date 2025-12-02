94de9b1403cc34436398aaaea998d96a54f8ecbd
rafaelvlt <rafaelmoonlight@hotmail.com>
2025-12-02 11:26:52 -0300

    docs & style: added/changed comments and indentation

--- src/systems/config_manager.c
+++ src/systems/config_manager.c
@@ -66,49 +66,48 @@ static void SetDefaultConfig(struct Systems* systems){
   systems->configManager.KeyMap.KeyWeaponGroups[3] = KEY_FOUR;  // 52
   systems->configManager.KeyMap.KeyWeaponGroups[4] = KEY_FIVE;  // 53
   systems->configManager.KeyMap.KeyShowMap = KEY_M; // 77
-  systems->configManager.KeyMap.KeyPause   = KEY_P; // 80
-  
+  systems->configManager.KeyMap.KeyPause   = KEY_ENTER;
   SaveConfigFile(systems);
 }
 
 static void SaveConfigFile(struct Systems* systems){
-    FILE* file = fopen(CONFIG_FILE, "w"); 
-    
-    if (file == NULL) {
-        TraceLog(LOG_WARNING, "Configuration file was not saved");
-        return;
-    }
+  FILE* file = fopen(CONFIG_FILE, "w"); 
 
-    fprintf(file, "[VIDEO]\n");
-    fprintf(file, "WIDTH=%d\n", (int)systems->configManager.screenResolution.x);
-    fprintf(file, "HEIGHT=%d\n", (int)systems->configManager.screenResolution.y);
-    fprintf(file, "FULLSCREEN=%d\n", systems->configManager.fullscreen ? 1 : 0);
-    
-    fprintf(file, "\n[AUDIO]\n");
-    fprintf(file, "VOLUME=%.2f\n", systems->configManager.audioVolume);
+  if (file == NULL) {
+    TraceLog(LOG_WARNING, "Configuration file was not saved");
+    return;
+  }
 
-    fprintf(file, "\n[PLAYER INPUT]\n");
-    fprintf(file, "KEY_MOVE_FORWARD=%d\n", systems->configManager.KeyMap.KeyMoveForward);
-    fprintf(file, "KEY_MOVE_BACKWARD=%d\n", systems->configManager.KeyMap.KeyMoveBackward);
-    fprintf(file, "KEY_TURN_LEFT=%d\n", systems->configManager.KeyMap.KeyTurnLeft);
-    fprintf(file, "KEY_TURN_RIGHT=%d\n", systems->configManager.KeyMap.KeyTurnRight);
-    
-    fprintf(file, "KEY_CENTER_TORSO=%d\n", systems->configManager.KeyMap.KeyCenterTorsoToLegs);
-    fprintf(file, "KEY_CENTER_LEGS=%d\n", systems->configManager.KeyMap.KeyCenterLegsToTorso);
-    
-    fprintf(file, "BTN_SHOOT=%d\n", systems->configManager.KeyMap.KeyShoot);
-    fprintf(file, "BTN_ZOOM=%d\n", systems->configManager.KeyMap.KeyZoom);
-    fprintf(file, "KEY_LOCK_TARGET=%d\n", systems->configManager.KeyMap.KeyLockTarget);
-    
-    for(int i = 0; i < 6; i++) {
-        fprintf(file, "KEY_WEAPON_%d=%d\n", i + 1, systems->configManager.KeyMap.KeyWeaponGroups[i]);
-    }
-    
-    fprintf(file, "KEY_SHOW_MAP=%d\n", systems->configManager.KeyMap.KeyShowMap);
-    fprintf(file, "KEY_PAUSE=%d\n", systems->configManager.KeyMap.KeyPause);
+  fprintf(file, "[VIDEO]\n");
+  fprintf(file, "WIDTH=%d\n", (int)systems->configManager.screenResolution.x);
+  fprintf(file, "HEIGHT=%d\n", (int)systems->configManager.screenResolution.y);
+  fprintf(file, "FULLSCREEN=%d\n", systems->configManager.fullscreen ? 1 : 0);
 
-    fclose(file);
-    TraceLog(LOG_INFO, "Configuration file saved succesfuly!");
+  fprintf(file, "\n[AUDIO]\n");
+  fprintf(file, "VOLUME=%.2f\n", systems->configManager.audioVolume);
+
+  fprintf(file, "\n[PLAYER INPUT]\n");
+  fprintf(file, "KEY_MOVE_FORWARD=%d\n", systems->configManager.KeyMap.KeyMoveForward);
+  fprintf(file, "KEY_MOVE_BACKWARD=%d\n", systems->configManager.KeyMap.KeyMoveBackward);
+  fprintf(file, "KEY_TURN_LEFT=%d\n", systems->configManager.KeyMap.KeyTurnLeft);
+  fprintf(file, "KEY_TURN_RIGHT=%d\n", systems->configManager.KeyMap.KeyTurnRight);
+
+  fprintf(file, "KEY_CENTER_TORSO=%d\n", systems->configManager.KeyMap.KeyCenterTorsoToLegs);
+  fprintf(file, "KEY_CENTER_LEGS=%d\n", systems->configManager.KeyMap.KeyCenterLegsToTorso);
+
+  fprintf(file, "BTN_SHOOT=%d\n", systems->configManager.KeyMap.KeyShoot);
+  fprintf(file, "BTN_ZOOM=%d\n", systems->configManager.KeyMap.KeyZoom);
+  fprintf(file, "KEY_LOCK_TARGET=%d\n", systems->configManager.KeyMap.KeyLockTarget);
+
+  for(int i = 0; i < 6; i++) {
+    fprintf(file, "KEY_WEAPON_%d=%d\n", i + 1, systems->configManager.KeyMap.KeyWeaponGroups[i]);
+  }
+
+  fprintf(file, "KEY_SHOW_MAP=%d\n", systems->configManager.KeyMap.KeyShowMap);
+  fprintf(file, "KEY_PAUSE=%d\n", systems->configManager.KeyMap.KeyPause);
+
+  fclose(file);
+  TraceLog(LOG_INFO, "Configuration file saved succesfuly!");
 }
 
 static void LoadConfigFile(struct Systems* systems){
